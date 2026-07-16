8cae65b58e0d6e6aa4e5a8dfba4ce4c283517a1d
aliibrar786 <alimuhammad2net@gmail.com>
2026-07-16 21:46:44 +0500

    Added semester loop, GPA and overall CGPA calculation

--- main.cpp
+++ main.cpp
@@ -19,13 +19,36 @@ int main()
   //DAY 1 
   int no_of_course ,credit_hour;
   string grade;
+  
+  int semester;
+   double overall_credit_hour = 0;
+  double overall_quality_point = 0;
+    double total_credit_hour = 0;
+  double total_quality_point = 0;
+
+  
+  
+
 
   cout<<"-------------------WELCOME TO CGPA CALCULATOR-----------------"<<endl;
+  cout<<"Enter number of semester: ";
+  cin>>semester;
+  for (int j = 1; j <= semester; j++)
+  {
+    // reset semester-level totals (NO 'double' here, just reset)
+     total_credit_hour = 0;
+   total_quality_point = 0;
+   cout << "=========== SEMESTER " << j << " ===========" << endl;
   cout<<"Enter the number of courses: ";
   cin>>no_of_course;
+  
+  
+  
 
   // for loop for each course  the grade and the credit hour
 
+  
+  
   for (int i = 1; i <=no_of_course; i++)
   {
       cout<<"***********COURSE"<<i<<"*****************"<<endl;
@@ -33,6 +56,73 @@ int main()
     cin>>credit_hour;
     cout<<"Enter the grade: ";
    cin>>grade;
+
+   float grade_point;
+  if(grade == "A+")
+  {
+    grade_point = 4.0;
+
   }
+  else if(grade == "A")
+  {
+    grade_point = 4.0;
+  }
+  else if(grade == "A-")
+  {
+    grade_point = 3.7;
+  }
+  else if(grade == "B+")
+  {
+    grade_point = 3.3;
+  }
+  else if(grade == "B")
+  {
+    grade_point = 3.0;
+  }
+  else if(grade == "B-")
+  {
+    grade_point = 2.7 ;
+  }
+  else if(grade == "C+")
+  {
+    grade_point = 2.3;
+  }
+  else if(grade == "C")
+  {
+    grade_point = 2.0;
+  }
+  else if(grade == "C-")
+  {
+    grade_point = 1.7;
+  }
+  else if(grade == "D")
+  {
+    grade_point = 1.0;
+  }
+ else{
+  grade_point = 0;
+ }
+ double quality_point = grade_point * credit_hour;
+ total_quality_point+=quality_point;
+ total_credit_hour+=credit_hour;
+  }
+
+
+  double GPA = total_quality_point/total_credit_hour;
+  cout<<"Semester "<<j<<" GPA: "<<GPA<<endl;
+    overall_quality_point += total_quality_point;    // ← YE LINE ADD KARO
+  overall_credit_hour += total_credit_hour; 
+}
+  double CGPA = overall_quality_point/overall_credit_hour;
+
+  cout<<"---------------------------------------------------"<<endl;
+  cout<<"OVERALL CGPA: "<<CGPA<<endl;
+
+  
+  
+
+
+
+ 
 }
   
